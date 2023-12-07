0a6baf3324ae9404bb1d0ae94934d5b261d04712
arif <arifur.tuc@gmail.com>
2023-12-07 14:40:12 +0100

    Registering MoveData model to admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MovieData
 
-# Register your models here.
+
+# Customizing the MovieDataAdmin to enhance the Django admin interface
+class MovieDataAdmin(admin.ModelAdmin):
+    list_display = ('name', 'duration', 'rating')
+    search_fields = ('name', )
+
+
+# Register the MovieData model
+admin.site.register(MovieData, MovieDataAdmin)
--- movies/models.py
+++ movies/models.py
@@ -6,3 +6,6 @@ class MovieData(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     rating = models.FloatField()
+
+    def __str__(self):
+        return self.name
