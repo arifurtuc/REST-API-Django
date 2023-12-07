0f6b16ab6dc4ced470d1f3a4d1b3227cc9bfdef5
arif <arifur.tuc@gmail.com>
2023-12-07 16:54:16 +0100

    Adding image to API

--- movies/models.py
+++ movies/models.py
@@ -7,6 +7,9 @@ class MovieData(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     movie_type = models.CharField(max_length=200, default='action')
+    image = models.ImageField(
+        upload_to='images/', default='images/None/Noimg.jpg'
+    )
 
     def __str__(self):
         return self.name
--- movies/serializers.py
+++ movies/serializers.py
@@ -5,7 +5,9 @@ from .models import MovieData
 # Define a serializer for the MovieData model to convert model instances
 # into JSON data
 class MovieSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True)
+
     class Meta:
         model = MovieData
         # Specify the fields to be included in the serialized representation
-        fields = ['id', 'name', 'duration', 'rating', 'movie_type']
+        fields = ['id', 'name', 'duration', 'rating', 'movie_type', 'image']
