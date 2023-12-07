c3ee1b301bca315dce45a64acaab64d85f7a0638
arif <arifur.tuc@gmail.com>
2023-12-07 15:55:17 +0100

    Adding serializer

---
+++ movies/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import MovieData
+
+
+# Define a serializer for the MovieData model to convert model instances
+# into JSON data
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieData
+        # Specify the fields to be included in the serialized representation
+        fields = ['id', 'name', 'duration', 'rating']
