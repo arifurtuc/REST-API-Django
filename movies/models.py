77b391e6b67d7b1c03e2cde9ee5056c29c456ab8
arif <arifur.tuc@gmail.com>
2023-12-07 14:31:35 +0100

    Adding movie model

---
+++ movies/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Define a Django Model to represent movie data in the database.
+class MovieData(models.Model):
+    name = models.CharField(max_length=200)
+    duration = models.FloatField()
+    rating = models.FloatField()
