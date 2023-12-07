af3d319eb5a9e1068e031e7cc9f92d5694d7b345
arif <arifur.tuc@gmail.com>
2023-12-07 16:15:37 +0100

    Adding views

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieSerializer
+from .models import MovieData
 
-# Create your views here.
+
+# Define a ViewSet for MovieData objects
+class MovieViewsSet(viewsets.ModelViewSet):
+    # Define the queryset to retrieve all MovieData
+    queryset = MovieData.objects.all()
+
+    # Specify the serializer class to be used
+    serializer_class = MovieSerializer
